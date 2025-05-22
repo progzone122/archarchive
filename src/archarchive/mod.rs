0c8cfdee715137fbdca2af7724ae7e0eed008014
jsst <105111211+jisstro@users.noreply.github.com>
2025-05-22 22:18:10 +0800

    Update mod.rs

--- src/archarchive/mod.rs
+++ src/archarchive/mod.rs
@@ -12,28 +12,31 @@ pub struct ArchArchive {
 impl ArchArchive {
     pub fn init() -> ArchArchive {
         Self {
-            year: "2025".to_string(),
-            month: "05".to_string(),
-            day: "15".to_string()
+            year: String::new(),
+            month: String::new(),
+            day: String::new()
         }
     }
     fn get_link(&self) -> String {
         format!("{}/{}/{}/{}/$repo/os/$arch", ENDPOINT, self.year, self.month, self.day)
     }
+    async fn select_date(&mut self) -> Result<()> {
+    let mut years = parser::parse_years().await?;
+    years.reverse();
+    self.year = menu::ask(years, "Select year: ");
 
+    let mut months = parser::parse_months(&self.year).await?;
+    months.reverse();
+    self.month = menu::ask(months, "Select month: ");
+
+    let mut days = parser::parse_days(&self.year, &self.month).await?;
+    days.reverse();
+    self.day = menu::ask(days, "Select day: ");
+
+    Ok(())
+    }
     pub async fn menu_run(&mut self) -> Result<String> {
-        let mut years: Vec<String> = parser::parse_years().await?;
-        years.reverse();
-        self.year = menu::ask(years, "Select year: ");
-
-        let mut months: Vec<String> = parser::parse_months(&self.year).await?;
-        months.reverse();
-        self.month = menu::ask(months, "Select month: ");
-
-        let mut days: Vec<String> = parser::parse_days(&self.year, &self.month).await?;
-        days.reverse();
-        self.day = menu::ask(days, "Select day: ");
-
-        Ok(self.get_link().to_string())
+        self.select_date().await?;
+        Ok(self.get_link())
     }
 }
